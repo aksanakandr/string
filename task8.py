535cd1b028e762e8146537d57baeb5ed5d8c59c2
aksanakandratovich <aksana.kandratovich@gmail.com>
2023-07-01 17:30:17 -0700

    task8_updated

--- task8.py
+++ task8.py
@@ -12,13 +12,14 @@ Task8.
 # Output: Max value = "2016". Nums are: (-7, 12, -24)
 """
 l1 = [-7, 1, 2, 0, 3, 12, -24, 5, -1, 6]
-l1.sort()
-nl = len(l1)
-nl2 = l1[0]*l1[1]*l1[nl-1]
-nl3 = l1[nl-1]*l1[nl-2]*l1[nl-3]
-l2 = (max(nl2, nl3))
-if l2 == nl2:
-    al = (l1[0], l1[1], l1[nl-1])
+l2=l1[:]
+l2.sort()
+list_len = len(l2)
+multi_case1 = l2[0] * l2[1] * l2[list_len - 1]
+multi_case2 = l2[list_len-1]*l2[list_len-2]*l2[list_len-3]
+max_mul = (max(multi_case1, multi_case2))
+if max_mul == multi_case2:
+    nums = (l2[0], l2[1], l2[list_len-1])
 else:
-    al = (l1[nl-1], l1[nl-2], l1[nl-3])
-print(f'Max value = {str(l2)} . Nums are: {al}')
+    nums = (l2[list_len-1], l2[list_len-2], l2[list_len-3])
+print(f'Max value = {str(max_mul )} . Nums are: {nums}')
